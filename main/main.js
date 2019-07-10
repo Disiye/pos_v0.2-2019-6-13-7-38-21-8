221b16f043c53d903a1b85166a668cd886ea4649
WENGEM <emilio.wang@oocl.com>
2019-07-10 08:21:32 +0800

    update

--- main/main.js
+++ main/main.js
@@ -18,14 +18,14 @@ function findCommodityByBarcode(inputs) {
 
 function findreceiptsByCommodity(commodity, lists){
   let result = '***<没钱赚商店>收据***\n';
-  var total = 0;
-  for(let key in lists){
+  let total = 0;
+  for(let key in commodity){
     for(let i = 0;i < lists.length;i++){
       if(key == lists[i].barcode){
-        result = result.concat('名称：' + lists[i].name + '，数量：'+ ItemList[key] + lists[i].unit + '，单价：' + lists[i].price.toFixed(2) +'(元)，小计：' +(lists[i].price * ItemList[key]).toFixed(2)+'(元)\n')
-       total += lists[i].price * ItemList[key];
+        result = result.concat('名称：' + lists[i].name + '，数量：'+ commodity[key] + lists[i].unit + '，单价：' + lists[i].price.toFixed(2) +'(元)，小计：' +(lists[i].price * commodity[key]).toFixed(2)+'(元)\n')
+       total += lists[i].price * commodity[key];
       }
     }
-  }
+}
   return result.concat('----------------------\n' + '总计：'+ total.toFixed(2) +'(元)\n' + '**********************');
 }
